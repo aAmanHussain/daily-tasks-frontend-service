976ec0403c526119e50b6b25287cbc6d33f32c6c
Aaman <aaman.hussain@impelsys.com>
2020-07-29 05:03:01 +0530

    Added service, guard and interceptor

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,23 @@
+﻿import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  constructor(private http: HttpClient) {}
+  register(user: any) {
+    return this.http.post(`${environment.baseAPIUrl}register`, user);
+  }
+  login(user: any) {
+    return this.http.post(`${environment.baseAPIUrl}login`, user);
+  }
+  getTodo() {
+    return this.http.get<any[]>(`${environment.baseAPIUrl}to-do`);
+  }
+  addTodo(data) {
+    return this.http.post<any[]>(`${environment.baseAPIUrl}to-do`, data);
+  }
+  deleteTodo(id: number) {
+    return this.http.delete(`${environment.baseAPIUrl}to-do/${id}`);
+  }
+}
